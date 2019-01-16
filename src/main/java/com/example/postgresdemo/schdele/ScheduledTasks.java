6e401f7c12f809d86e1badcf82fa477958994d16
ankoks <koksharov.work@gmail.com>
2019-01-16 11:47:58 +0300

    Add scheduling with cron @Value param

--- src/main/java/com/example/postgresdemo/schdele/ScheduledTasks.java
+++ src/main/java/com/example/postgresdemo/schdele/ScheduledTasks.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 /**
  * User: ankoks
@@ -18,8 +18,80 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "${application.cron.value}")
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+
+    public static void main(String[] args) {
+        List<PoJo> pojos = new ArrayList<>();
+        pojos.add(new PoJo("Anton", 26, "d", false));
+        pojos.add(new PoJo("Michail", 31, "d", false));
+        pojos.add(new PoJo("Alex", 15, "some signature 3", false));
+        pojos.add(new PoJo("Tolya", 15, null, true));
+
+        Optional<PoJo> first = pojos.stream()
+                .filter(s -> s.getSignature() == null || s.getSignature().isEmpty())
+                .filter(s -> !s.isLastCurator()).min(Comparator.comparing(PoJo::getAge));
+
+        System.out.println(first);
+
+        System.out.println(first.isPresent());
+    }
+
+    static class PoJo {
+        private String name;
+        private Integer age;
+        private String signature;
+        private boolean lastCurator;
+
+        public PoJo(String name, Integer age, String signature, boolean lastCurator) {
+            this.name = name;
+            this.age = age;
+            this.signature = signature;
+            this.lastCurator = lastCurator;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        public String getSignature() {
+            return signature;
+        }
+
+        public void setSignature(String signature) {
+            this.signature = signature;
+        }
+
+        public boolean isLastCurator() {
+            return lastCurator;
+        }
+
+        public void setLastCurator(boolean lastCurator) {
+            this.lastCurator = lastCurator;
+        }
+
+        @Override
+        public String toString() {
+            return "PoJo{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", signature='" + signature + '\'' +
+                    ", lastCurator=" + lastCurator +
+                    '}';
+        }
     }
 }
