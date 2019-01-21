f36ea434081b19e9596c869958203b71d8bf68c4
ankoks <koksharov.work@gmail.com>
2019-01-21 15:07:24 +0300

    add Spring security inMemoryAuth

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -1,15 +1,26 @@
 package com.example.postgresdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class PostgresDemoApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(PostgresDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresDemoApplication.class, args);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .withUser("user").password("user").roles("USER");
+    }
 }
